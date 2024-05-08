b6a52d733370ed6c8a9666eedd4828ac4e25704c
ZhaohanGao <gaozhaohan22@gmail.com>
2024-05-08 23:14:17 +0800

    first commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js"
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads
+
+app.use("/api/auth", authRoutes)
+
+// app.get("/", (req, res) =>{
+//     // root route http://localhost:5000/
+//     res.send("11221");
+// });
+
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`server is running on ${PORT}`)
+});
