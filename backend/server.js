aaa45f1a47f9de5372103697d611db74edb79690
ZhaohanGao <gaozhaohan22@gmail.com>
2024-05-09 22:53:26 +0800

    5.9

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -11,7 +12,8 @@ dotenv.config();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 // app.get("/", (req, res) =>{
 //     // root route http://localhost:5000/
